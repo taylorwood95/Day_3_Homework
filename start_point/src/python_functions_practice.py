be684f10a65a5ffc113d61b479eeb3c798cb5921
taylorwood95@hotmail.com <taylorwood95@hotmail.com>
2023-01-11 16:51:31 +0000

    first commit

---
+++ start_point/src/python_functions_practice.py
@@ -0,0 +1,65 @@
+def return_10():
+    return 10 
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2 
+
+def multiply(num1, num2):
+    return num1 * num2
+
+
+def divide(num1, num2):
+    return num1 / num2
+
+
+def length_of_string(string_length):
+    return len(string_length)
+
+
+def join_string(string_1, string_2):
+    return string_1 + string_2 
+
+
+def add_string_as_number(num1, num2):
+    return int(num1) + int(num2)
+
+def number_to_full_month_name(month):
+    if month == 1:
+        return "January"
+    elif month == 2:
+        return "Febuary"
+    elif month == 3:
+        return "March"
+    elif month == 9:
+        return "September"
+
+
+def number_to_short_month_name (month):
+    if month == 1:
+        return 'Jan'
+    elif month == 4:
+        return "Apr"
+    elif month == 10:
+        return 'Oct'
+        
+
+def volume_of_cube(length):
+    return length ** 3
+
+def reverse_string(string):
+    return string[::-1]
+
+
+def fahrenheit_to_celsius(num):
+    return (num -32) * 1.8
+
+
+    
+
+     
+
+
+
